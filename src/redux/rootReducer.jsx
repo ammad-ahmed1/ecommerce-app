0ea48172833354ce4521d8074289a17eee4dd7a9
Ammad-Cherrybyte <127683502+Ammad-Cherrybyte@users.noreply.github.com>
2024-03-20 15:09:34 +0500

    payment-int-started

--- src/redux/rootReducer.jsx
+++ src/redux/rootReducer.jsx
@@ -3,10 +3,12 @@ import authSlice from "./slice/authSlice";
 import productSlice from "./slice/productSlice";
 import filterSlice from "./slice/filterSlice";
 import cartSlice from "./slice/cartSlice";
+import checkoutSlice from "./slice/checkoutSlice";
 
 export const rootReducer = combineReducers({
   auth: authSlice,
   product: productSlice,
   filter: filterSlice,
   cart: cartSlice,
+  checkout: checkoutSlice,
 });
